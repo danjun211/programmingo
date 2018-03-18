32337aa7c6f0610c0fda0abedbc401d80ce9967d
LeeJunyeol <chljy33@naver.com>
2018-03-18 12:45:39 +0900

    디자인

--- src/views/Courses/index.js
+++ src/views/Courses/index.js
@@ -1,68 +1,95 @@
 import React, { Component } from "react";
 import Nav from "../../components/Nav";
 import Footer from "../../components/Footer";
-import { Button } from "reactstrap";
+import {
+  Button,
+  Card,
+  CardBody,
+  CardTitle,
+  CardSubtitle,
+  CardText,
+  CardImg
+} from "reactstrap";
 import "./styles.css";
 import "./courselist.css";
 
 class Courses extends Component {
   render() {
     console.log(this.props.match.params.id);
-    return (
-      <div className="wrapper">
+    return <div className="wrapper">
         <Nav />
-        <div>
-            <div className="courselist">
-              <div className="courselist_header">
-                <div className="header_column">
-                  <i className="fas fa-plug fa-3x" />
-                </div>
-                <div className="header_column">
-                  <h1>기초1</h1>
-                  <span>1/3 레슨 시작</span>
-                </div>
-                <div className="header_column">시험보고 건너뛰기</div>
+        <div className="mobile_course">
+          <Card>
+            <CardBody>
+              <CardTitle>기초1</CardTitle>
+              <Button>과정 시작</Button>
+            </CardBody>
+          </Card>
+          <Card>
+            <CardBody>
+              <CardTitle>기초2</CardTitle>
+              <Button>과정 시작</Button>
+            </CardBody>
+          </Card>
+          <Card>
+            <CardBody>
+              <CardTitle>기초3</CardTitle>
+              <Button>과정 시작</Button>
+            </CardBody>
+          </Card>
+        </div>
+
+        <div className="course_wrapper course">
+          <div className="courselist">
+            <div className="courselist_header">
+              <div className="header_column">
+                <i className="fas fa-plug fa-3x" />
               </div>
-              <div className="courselist_body">
-                <div className="body_column">
-                  <div className="body_column_title">
-                    <h3>1/3 레슨</h3>
-                  </div>
-                  <div className="body_column_middle">
-                    <span>blah balh</span>
-                  </div>
-                  <div className="body_column_bottom">
-                    <div className="body_column_btn">시 작</div>
-                  </div>
+              <div className="header_column">
+                <h1>기초1</h1>
+                <span>1/3 레슨 시작</span>
+              </div>
+              <div className="header_column">시험보고 건너뛰기</div>
+            </div>
+            <div className="courselist_body">
+              <div className="body_column">
+                <div className="body_column_title">
+                  <h3>1/3 레슨</h3>
                 </div>
-                <div className="body_column">
-                  <div className="body_column_title">
-                    <h3>2/3 레슨</h3>
-                  </div>
-                  <div className="body_column_middle">
-                    <span>blah balh</span>
-                  </div>
-                  <div className="body_column_bottom">
-                    <div className="body_column_btn">잠 김</div>
-                  </div>
+                <div className="body_column_middle">
+                  <span>blah balh</span>
                 </div>
-                <div className="body_column">
-                  <div className="body_column_title">
-                    <h3>3/3 레슨</h3>
-                  </div>
-                  <div className="body_column_middle">
-                    <span>blah balh</span>
-                  </div>
-                  <div className="body_column_bottom">
-                    <div className="body_column_btn">잠 김</div>
-                  </div>
+                <div className="body_column_bottom">
+                  <div className="body_column_btn">시 작</div>
+                </div>
+              </div>
+              <div className="body_column">
+                <div className="body_column_title">
+                  <h3>2/3 레슨</h3>
+                </div>
+                <div className="body_column_middle">
+                  <span>blah balh</span>
+                </div>
+                <div className="body_column_bottom">
+                  <div className="body_column_btn">잠 김</div>
+                </div>
+              </div>
+              <div className="body_column">
+                <div className="body_column_title">
+                  <h3>3/3 레슨</h3>
+                </div>
+                <div className="body_column_middle">
+                  <span>blah balh</span>
+                </div>
+                <div className="body_column_bottom">
+                  <div className="body_column_btn">잠 김</div>
                 </div>
               </div>
             </div>
+          </div>
         </div>
         <Footer />
-      </div>
-    );
+      </div>;
   }
 }
 
