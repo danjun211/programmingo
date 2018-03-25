30f4bbbe134f5a0da93f7ef9d3e155ed0288122c
LeeJunyeol <chljy33@naver.com>
2018-03-25 15:59:23 +0900

    add public url and homepage

--- src/App.js
+++ src/App.js
@@ -10,9 +10,9 @@ class App extends Component {
   render() {
     return <div>
         <link href="https://use.fontawesome.com/releases/v5.0.8/css/all.css" rel="stylesheet" />
-        <Route exact path="/" component={Main} />
-        <Route path="/courses/:id" component={Courses} />
-        <Route path="/questionare/:categoryId/" component={Questionare} />
+        <Route exact path={process.env.PUBLIC_URL + "/"} component={Main} />
+        <Route path={process.env.PUBLIC_URL + "/courses/:id"} component={Courses} />
+        <Route path={process.env.PUBLIC_URL + "/questionare/:categoryId/"} component={Questionare} />
       </div>;
   }
 }
